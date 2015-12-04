13b1ab6f8f3bd6c3bda0e1fe1fbdfa520624b86a
Harry Wagstaff <fred.the.fish@gmail.com>
2015-12-04 18:53:10 +0000

    Add TODO to enable MMU in hot memory test

--- benchmarks/memory/memory-hot.c
+++ benchmarks/memory/memory-hot.c
@@ -9,6 +9,8 @@ static void kernel()
 	uint32_t total_iterations = BENCHMARK_ITERATIONS * MEMORY_BENCHMARK_ITERATIONS;
 	uint32_t i;
 	
+	//TODO: Enable MMU
+	
 	static volatile uint32_t value = 0;
 	
 	for(i = 0; i < total_iterations; ++i) {
