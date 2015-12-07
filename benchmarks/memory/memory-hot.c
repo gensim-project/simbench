3454d140bc1cb81128631cbd8fecb19573e59d9e
Harry Wagstaff <fred.the.fish@gmail.com>
2015-12-07 18:19:03 +0000

    Funroll the memory access loop a bit more, add both MMU and non-MMU versions of test

--- benchmarks/memory/memory-hot.c
+++ benchmarks/memory/memory-hot.c
@@ -1,4 +1,5 @@
 #include "benchmark.h"
+#include "mem.h"
 
 #include <stdint.h>
 
@@ -24,13 +25,71 @@ static void kernel()
 		value = value;
 		value = value;
 		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
 	}
 }
 
+static void kernel_mmu()
+{
+	uint32_t total_iterations = BENCHMARK_ITERATIONS * MEMORY_BENCHMARK_ITERATIONS;
+	uint32_t i;
+	
+	mem_init();
+	
+	static volatile uint32_t value = 0;
+	mem_create_page_mapping(&value, &value);
+	
+	mem_tlb_flush();
+	mem_mmu_enable();
+	
+	for(i = 0; i < total_iterations; ++i) {
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+		value = value;
+	}
+
+	mem_mmu_disable();
+	mem_tlb_flush();
+	mem_reset();
+}
+
 static benchmark_t bmark = {
-	.name="Memory-Hot",
+	.name="Memory-Hot-NoMMU",
 	.category="Memory",
 	.kernel=kernel
 };
 
-REG_BENCHMARK(&bmark);
+static benchmark_t bmark_mmu = {
+	.name="Memory-Hot-MMU",
+	.category="Memory",
+	.kernel=kernel_mmu
+};
+
+REG_BENCHMARK(bmark);
+REG_BENCHMARK(bmark_mmu);
