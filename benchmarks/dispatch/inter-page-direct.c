0d12a13c9e9809b987c91d66ba8c861945ce09dd
Harry Wagstaff <fred.the.fish@gmail.com>
2015-12-08 15:57:43 +0000

    Add some new code dispatch benchmarks

---
+++ benchmarks/dispatch/inter-page-direct.c
@@ -0,0 +1,113 @@
+#include "benchmark.h"
+
+#include <stdint.h>
+
+#define DISPATCH_ITERATIONS 50000
+
+#define NOREORDER 
+#define NOINLINE __attribute__((noinline))
+asm (".align 12\n") NOREORDER;
+
+static volatile uint32_t value;
+
+static void NOINLINE fn1()
+{
+	asm volatile ("");
+	value = 1;
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn2()
+{
+	asm volatile ("");
+	value = 2;
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn3(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn1();
+	else fn2();
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn4()
+{
+	asm volatile ("");
+	fn1();
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn5(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn3(x >> 1);
+	else fn4();
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn6(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn3(x >> 1);
+	else fn1();
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn7(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn6(x >> 1);
+	else fn5(x >> 1);
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn8(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn1();
+	else fn3(x >> 1);
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn9(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn8(x >> 1);
+	else fn5(x >> 1);
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn10(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn8(x >> 1);
+	else fn7(x >> 1);
+}
+
+asm (".align 12\n") NOREORDER;
+static void NOINLINE fn11(int x)
+{
+	asm volatile ("");
+	if(x & 1) fn9(x >> 1);
+	else fn10(x >> 1);
+}
+
+asm (".align 12\n") NOREORDER;
+static void kernel()
+{
+	uint64_t total_iterations = BENCHMARK_ITERATIONS * DISPATCH_ITERATIONS;
+	uint64_t i;
+	for(i=0; i < total_iterations; ++i)
+	{
+		fn11(i);
+	}
+}
+
+static benchmark_t bmark = {
+	.name="Inter-Page-Direct",
+	.category="Dispatch",
+	.kernel=kernel
+};
+REG_BENCHMARK(bmark);
---
+++ benchmarks/dispatch/inter-page-indirect.c
@@ -0,0 +1,56 @@
+#include "benchmark.h"
+
+#include <stdint.h>
+
+#define NOREORDER 
+//#define NOREORDER __attribute__((no_reorder))
+
+#define DISPATCH_ITERATIONS 100
+
+asm (".align 12\n") NOREORDER;
+
+#define leaf_fn(x) asm (".align 12\n") NOREORDER; static void NOREORDER fn ## x() {}
+
+#define FN_TABLE_SIZE 16
+static benchmark_kernel_t fn_table[];
+
+static void NOREORDER dispatch (int i) {
+	while(i--) {
+		fn_table[i % FN_TABLE_SIZE]();
+	}
+}
+
+leaf_fn(1)
+leaf_fn(2)
+leaf_fn(3)
+leaf_fn(4)
+leaf_fn(5)
+leaf_fn(6)
+leaf_fn(7)
+leaf_fn(8)
+leaf_fn(9)
+leaf_fn(10)
+leaf_fn(11)
+leaf_fn(12)
+leaf_fn(13)
+leaf_fn(14)
+leaf_fn(15)
+leaf_fn(16)
+
+static benchmark_kernel_t fn_table[FN_TABLE_SIZE] = { fn1, fn2, fn3, fn4, fn5, fn6, fn7, fn8, fn9, fn10, fn11, fn12, fn13, fn14, fn15, fn16 };
+
+static void kernel()
+{
+	uint32_t total_iterations = BENCHMARK_ITERATIONS * DISPATCH_ITERATIONS;
+	int i = 0;
+	for(i = 0; i < total_iterations; ++i) {
+		dispatch(1000);
+	}
+}
+
+static benchmark_t bmark = {
+	.name="Inter-Page-Indirect",
+	.category="Dispatch",
+	.kernel=kernel
+};
+REG_BENCHMARK(bmark);
