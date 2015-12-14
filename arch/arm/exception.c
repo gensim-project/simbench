5827be1d01c6aeaa60e6652c6677b7b24202fec6
Harry Wagstaff <fred.the.fish@gmail.com>
2015-12-14 17:51:06 +0000

    Add two exception benchmarks and infrastructure to support them

---
+++ arch/arm/exception.c
@@ -0,0 +1,35 @@
+#include "arch.h"
+
+extern uint32_t vectors_start;
+extern uint32_t pabt_handler;
+extern uint32_t dabt_handler;
+extern void pabt_return();
+extern void dabt_skip();
+extern void default_handler();
+
+// The vectors have been copied into the low page so need to do some address calculation
+static void install_pabt(uint32_t pabt_location)
+{
+	uint32_t vector_location = (uint32_t)&pabt_handler - (uint32_t)&vectors_start;
+	*(uint32_t*)vector_location = pabt_location;
+}
+static void install_dabt(uint32_t dabt_location)
+{
+	uint32_t vector_location = (uint32_t)&dabt_handler - (uint32_t)&vectors_start;
+	*(uint32_t*)vector_location = dabt_location;
+}
+
+void arch_ifault_install_return()
+{
+	install_pabt((uint32_t)pabt_return);
+}
+
+void arch_ifault_install_break()
+{
+	install_pabt((uint32_t)default_handler);
+}
+
+void arch_dfault_install_skip()
+{
+	install_dabt((uint32_t)dabt_skip);
+}
--- include/arch.h
+++ include/arch.h
@@ -6,6 +6,11 @@
 extern void arch_init();
 
 extern void arch_code_flush(size_t address);
-uint32_t arch_nonpriviliged_write(uint32_t *ptr);
+extern uint32_t arch_nonpriviliged_write(uint32_t *ptr);
+
+extern void arch_ifault_install_return();
+extern void arch_ifault_install_break();
+
+extern void arch_dfault_install_skip();
 
 #endif
