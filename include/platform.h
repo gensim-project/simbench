89900f1f306bbd4cfa5c3af93ddcc805bf3bc41d
Harry Wagstaff <fred.the.fish@gmail.com>
2015-12-14 18:44:58 +0000

    Add infrastructure and benchmark for device accesses

--- include/platform.h
+++ include/platform.h
@@ -1,7 +1,15 @@
 #ifndef PLATFORM_PLATFORM_H
 #define PLATFORM_PLATFORM_H
 
+#include "define.h"
+
 extern void platform_init();
 extern void platform_shutdown();
+
+// Return a pointer to an unused device which is safe to write to. This will be used for evaluating
+// device access performance
+extern uint32_t* platform_get_null_devptr();
+// Return a value which is safe to write to the pointer returned by platform_get_null_devptr().
+extern uint32_t platform_get_null_devval();
 
 #endif
--- platform/arm/versatile/mem.c
+++ platform/arm/versatile/mem.c
@@ -7,10 +7,16 @@ static phys_mem_info_t phys_info = {
 	.next_mem = 0
 };
 
+static phys_mem_info_t device_sp810_info = {
+	.phys_mem_start = 0x10000000,
+	.phys_mem_end   = 0x10000fff,
+	.next_mem       = 0
+};
+
 static phys_mem_info_t device_uart_info = {
 	.phys_mem_start = UART0_MEM_START,
 	.phys_mem_end   = UART0_MEM_END,
-	.next_mem = 0
+	.next_mem = &device_sp810_info
 };
 
 const phys_mem_info_t *mem_get_phys_info()
