01bf4f0666f2c6f0569ced49128bcf24bf3a445e
Harry Wagstaff <fred.the.fish@gmail.com>
2016-01-11 13:57:00 +0000

    Add support for measuring control kernels to the host program

--- host/main.c
+++ host/main.c
@@ -24,20 +24,15 @@ int read_start()
 	return 0;
 }
 
-int read_benchmark_begin()
-{
-	if(fgetc(stdin) != '[') return 1;
-	return 0;
-}
-
-int read_benchmark_end()
+static char read_timing_start_char()
 {
 	while(1) {
 		char c = fgetc(stdin);
 		
 		switch(c) {
-			case ']': 
-				return 0;
+			case '{': 
+			case '[': 
+				return c;
 			case '|':
 			case '/':
 			case '-':
@@ -46,34 +41,95 @@ int read_benchmark_end()
 			case 0x08:
 				continue;
 			default: 
-				return 1;
+				return 0;
 		}
 	}
+}
+
+static char read_timing_end_char(char target)
+{
+	while(1) {
+		char c = fgetc(stdin);
+		if(c == target) return target;
+		
+		switch(c) {
+			case '|':
+			case '/':
+			case '-':
+			case '\\':
+			case ' ':
+			case 0x08:
+				continue;
+			default: 
+				return 0;
+		}
+	}
+}
+
+typedef struct {
+	uint64_t control_ticks;
+	uint64_t benchmark_ticks;
+} benchmark_times_t;
+
+static uint64_t get_ticks_difference(struct timespec start, struct timespec end)
+{
+	uint64_t start_ticks = start.tv_sec*1000000000 + start.tv_nsec;
+	uint64_t end_ticks =   end.tv_sec*1000000000 + end.tv_nsec;
+	
+	uint64_t ticks_taken = end_ticks - start_ticks;
+	
+	return ticks_taken;
+}
+
+static benchmark_times_t time_benchmarks()
+{
+	benchmark_times_t times;
+	struct timespec start_time, end_time;
+	
+	char time_start_char = read_timing_start_char();
+	if(time_start_char == '{') {
+		clock_gettime(CLOCK_MONOTONIC, &start_time);
+		read_timing_end_char('}');
+		clock_gettime(CLOCK_MONOTONIC, &end_time);
+		
+		times.control_ticks = get_ticks_difference(start_time, end_time);
+		
+		time_start_char = read_timing_start_char();
+	} else {
+		times.control_ticks = 0;
+	}
+	
+	if(time_start_char == '[') {
+		clock_gettime(CLOCK_MONOTONIC, &start_time);
+		read_timing_end_char(']');
+		clock_gettime(CLOCK_MONOTONIC, &end_time);
+		
+		times.benchmark_ticks = get_ticks_difference(start_time, end_time);		
+	}
+	
+	return times;
 }
 
 int read_benchmark() 
 {
 	char category[128];
 	char benchmark[128];
+	uint64_t iterations;
 	
 	struct timespec start_time, end_time;
 	
-	if(fscanf(stdin, "%s - %s ", category, benchmark) != 2) return 1;
+	if(fscanf(stdin, "%s - %s %lx ", category, benchmark, &iterations) != 3) return 1;
 	
-	printf("%s - %s", category, benchmark);
+	printf("%s - %s %lx ", category, benchmark, iterations);
 	
-	if(read_benchmark_begin()) return 1;
-	clock_gettime(CLOCK_MONOTONIC, &start_time);
-	if(read_benchmark_end()) return 1;
-	clock_gettime(CLOCK_MONOTONIC, &end_time);
+	benchmark_times_t times = time_benchmarks();
 	
-	uint64_t start_ticks = start_time.tv_sec*1000000000 + start_time.tv_nsec;
-	uint64_t end_ticks =   end_time.tv_sec*1000000000 + end_time.tv_nsec;
 	
-	uint64_t ticks_taken = end_ticks - start_ticks;
-	double seconds_taken = ticks_taken / 1000000000.0;
+	double control_seconds = times.control_ticks / 1000000000.0;
+	double benchmarks_seconds = times.benchmark_ticks / 1000000000.0;
+	if(control_seconds) printf(" %lf", control_seconds);
 	
-	printf(" %lf\r\n", seconds_taken);
+	printf(" %lf\r\n", benchmarks_seconds);
 	
 	return 0;
 }
