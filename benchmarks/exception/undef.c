cdefd3e3cfd666ebd2a5ef142e37206ad7af0ec8
Harry Wagstaff <fred.the.fish@gmail.com>
2016-01-13 15:26:24 +0000

    Add an undefined instruction handling benchmark

---
+++ benchmarks/exception/undef.c
@@ -0,0 +1,44 @@
+#include "arch.h"
+#include "benchmark.h"
+#include "debug.h"
+#include "mem.h"
+
+#define EXCEPTION_BENCHMARK_ITERATIONS 2500
+
+__align12;
+
+static void ALIGN kernel()
+{
+	uint64_t i, total_iterations = BENCHMARK_ITERATIONS * EXCEPTION_BENCHMARK_ITERATIONS;
+	
+	debug_spinner_start(EXCEPTION_BENCHMARK_ITERATIONS);
+	
+	for(i=0; i < total_iterations; ++i) {
+		debug_spinner();
+		arch_undefined_instruction();
+	}
+}
+
+static void kernel_init()
+{
+	mem_init();
+	mem_mmu_enable();
+	mem_tlb_flush();
+	
+	arch_undef_install_skip();
+}
+static void kernel_cleanup()
+{
+	mem_mmu_disable();
+	mem_tlb_flush();
+	mem_reset();
+}
+
+DEFINE_BENCHMARK(undef) = {
+	.name="Undef-Instruction",
+	.category="Exception",
+	.kernel_init=kernel_init,
+	.kernel=kernel,
+	.kernel_cleanup=kernel_cleanup,
+	.iteration_count = EXCEPTION_BENCHMARK_ITERATIONS * BENCHMARK_ITERATIONS
+};
