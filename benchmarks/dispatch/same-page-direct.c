c3f9dfdbde3445ba1f7c002bb3e560ad15941b2a
Harry Wagstaff <fred.the.fish@gmail.com>
2016-03-23 16:11:01 +0000

    Modify same-page-direct benchmark to remove non-control-flow related ops

--- benchmarks/dispatch/same-page-direct.c
+++ benchmarks/dispatch/same-page-direct.c
@@ -10,83 +10,17 @@ __align12 NOREORDER;
 
 static volatile uint32_t value;
 
-static void NOINLINE fn1()
-{
-	asm volatile ("");
-	value = 1;
-}
-
-static void NOINLINE fn2()
-{
-	asm volatile ("");
-	value = 2;
-}
-
-static void NOINLINE fn3(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn1();
-	else fn2();
-}
-
-static void NOINLINE fn4()
-{
-	asm volatile ("");
-	fn1();
-}
-
-static void NOINLINE fn5(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn3(x >> 1);
-	else fn4();
-}
-
-static void NOINLINE fn6(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn3(x >> 1);
-	else fn1();
-}
-
-static void NOINLINE fn7(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn6(x >> 1);
-	else fn5(x >> 1);
-}
-
-
-static void NOINLINE fn8(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn1();
-	else fn3(x >> 1);
-}
-
-
-static void NOINLINE fn9(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn8(x >> 1);
-	else fn5(x >> 1);
-}
-
-
-static void NOINLINE fn10(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn8(x >> 1);
-	else fn7(x >> 1);
-}
-
-
-static void NOINLINE fn11(int x)
-{
-	asm volatile ("");
-	if(x & 1) fn9(x >> 1);
-	else fn10(x >> 1);
-}
+static void NOINLINE fn1() { asm volatile ("":::"memory"); }
+static void NOINLINE fn2() { asm volatile ("":::"memory");fn1(); }
+static void NOINLINE fn3() { asm volatile ("":::"memory");fn2(); }
+static void NOINLINE fn4() { asm volatile ("":::"memory");fn3(); }
+static void NOINLINE fn5() { asm volatile ("":::"memory");fn4(); }
+static void NOINLINE fn6() { asm volatile ("":::"memory");fn5(); }
+static void NOINLINE fn7() { asm volatile ("":::"memory");fn6(); }
+static void NOINLINE fn8() { asm volatile ("":::"memory");fn7(); }
+static void NOINLINE fn9() { asm volatile ("":::"memory");fn8(); }
+static void NOINLINE fn10() { asm volatile ("":::"memory");fn9(); }
+static void NOINLINE fn11() { asm volatile ("":::"memory");fn10(); }
 
 static void kernel()
 {
